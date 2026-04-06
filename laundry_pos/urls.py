92e5f52b42f61354b3f1f111c46d2edd43f48886
remaruru <earlangeloroldan18@gmail.com>
2026-04-07 00:18:05 +0800

    Fix 404 for media/QR images in production when DEBUG=False

--- laundry_pos/urls.py
+++ laundry_pos/urls.py
@@ -24,5 +24,14 @@ urlpatterns = [
     path('', include('core.urls')),
 ]
 
+from django.urls import re_path
+from django.views.static import serve
+
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+else:
+    # Force serve media files in production (Render) for QR codes
+    urlpatterns += [
+        re_path(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT}),
+    ]
+
